2e8d57173321e7679c69fa4cd4a79261e83dd1a2
Daniil Sheftelevich <shftlvch@gmail.com>
2017-12-28 13:14:47 +0300

    ok, alpha

--- src/components/Popover.js
+++ src/components/Popover.js
@@ -2,12 +2,12 @@ import React, {Component} from "react";
 
 import "./Popover.css";
 import Share from "./Share";
+import template from '../assets/template-placeholder-3.jpg'
 
 
 class Popover extends Component {
     constructor(props) {
         super(props);
-
 
 
         this.handleClose = this.handleClose.bind(this);
@@ -19,7 +19,8 @@ class Popover extends Component {
 
 
     render() {
-        const props = this.props;
+        const props = this.props,
+            name = props.name.charAt(0).toUpperCase() + props.name.slice(1);
 
         if (typeof document !== 'undefined') {
             if (props.open) {
@@ -31,22 +32,40 @@ class Popover extends Component {
 
         return (
             <div className={"modal-over " + (props.open ? 'modal-over_open' : '')}>
-                <div className="container">
+                <div className="modal-over__inner container">
                     <div className="row">
                         <div className="col-lg-6 offset-lg-3">
-                            {props.name}, спасибо!
-                            <img src={props.image} className="img img-fluid"/>
-                            <div className="result__share-label">Поделитесь мотивацией:</div>
-                            <Share
-                                className="result__share"
-                                title={`я ${props.role} #лагерь_вверх — Центр «Вверх»`}
-                                url={`http://bright.vverh.su/view/${props.hash}`}
-                                image={props.image}
-                            />
+                            <div className="result">
+                                <div className="result__header">{name}, спасибо!</div>
+                                <div className="result__img">
+                                    <img src={template} className="result__template img img-fluid"/>
+                                    <img src={props.image} className="result__final-img img img-fluid"/>
+                                </div>
+                                <div className="result__share-label">Поделитесь мотивацией:</div>
+                                <Share
+                                    className="result__share"
+                                    title={`я ${props.role} #лагерь_вверх — Центр «Вверх»`}
+                                    url={`http://belskoe.vverh.su/view/${props.hash}`}
+                                    image={props.image}
+                                />
+                                <div className="result__btns">
+                                    <a href="https://vverh.su/campaign/help/"
+                                       target="_blank" rel="noopener noreferrer"
+                                       className="col-lg-5 btn btn_donate result__donate">
+                                        <i className="fa fa-heart-o" aria-hidden="true"/>
+                                        Помочь «Вверху»
+                                    </a>
+                                    <button className="col-lg-5 offset-lg-2 btn btn-light result__reset" onClick={this.handleClose}>
+                                        <i className="fa fa-refresh" aria-hidden="true"/>
+                                        Начать заново
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div onClick={this.handleClose} className="modal-over__close"><i class="fa fa-times" aria-hidden="true"></i></div>
+                <div onClick={this.handleClose} className="modal-over__close"><i class="fa fa-times"
+                                                                                 aria-hidden="true"></i></div>
             </div>
         )
     }
--- src/components/View.js
+++ src/components/View.js
@@ -52,38 +52,30 @@ export default class View extends Component {
     }
 
     render() {
-        console.log('render');
-        console.log(this.props);
-        console.log(this.state);
-        console.log(this.props.staticContext);
-
-
-        let meta, view, reason = '', pic = '';
-
-
+        let meta, view, role = '', pic = '';
 
         if (this.props.staticContext || this.state.static) {
             console.log('has staticContext');
 
             view = this.props.staticContext ? this.props.staticContext.data.view: this.state.static;
-            reason = '';
+            role = '';
             pic = false;
 
             if (typeof view.then !== 'function') {
                 console.log(view);
 
-                reason = view.reason;
+                role = view.role;
                 pic = view.pic;
             }
 
             meta = {
-                title: `#мне_не_серо_когда ${reason} — Центр «Вверх»`,
-                description: 'Продержитесь до праздников, расскажите друзьям что помогает вам бороться с серостью и не унывать эти 7 месяцев в году.',
-                canonical: `http://bright.vverh.su/view/${this.props.match.params.hash}`,
+                title: `я ${role} #лагерь_вверх — Центр «Вверх»`,
+                description: 'Расскажите друзьям про проект #лагерь_вверх, может быть, благодаря вам у детей из интерната в Бельском-Устье будет больше поводов для улыбок.',
+                canonical: `http://belskoe.vverh.su/view/${this.props.match.params.hash}`,
                 meta: {
                     charSet: 'utf-8',
                     name: {
-                        keywords: '#мне_не_серо_когда, Вверх, благотворительность, свет, несерость, зима, позитивное мышление, Центр равных возможностей для детей-сирот'
+                        keywords: '#лагерь_вверх, Вверх, благотворительность, Центр равных возможностей для детей-сирот'
                     },
                     property: {
                         'og:image': pic ? pic.url : '',
@@ -99,9 +91,7 @@ export default class View extends Component {
 
         return (
             <DocumentMeta {...meta}>
-
-                <h2>{reason}</h2>
-                {/*<Redirect to="/" push/>*/}
+                <h1>{`я ${role} #лагерь_вверх — Центр «Вверх»`}</h1>
             </DocumentMeta>
 
         );
